0eac66589f1fdfeb89ac0f19fc7e55683b6a49a5
bea <bea@bea-Virtual>
2014-04-27 15:49:47 -0400

    Se pasa por alto autenticacion para pruebas
    Change-Id: If9d7831303f447b430b56a0166195f7fe1e80c63

--- src/py/com/quimpar/session/Authenticator.java
+++ src/py/com/quimpar/session/Authenticator.java
@@ -16,87 +16,97 @@ import org.jboss.seam.log.Log;
 import org.jboss.seam.security.Credentials;
 import org.jboss.seam.security.Identity;
 
-
-
 @Name("authenticator")
 @Scope(ScopeType.SESSION)
 public class Authenticator {
-  
-  @Logger
-  private Log log;
-  @In
-  Identity identity;
-  @In
-  Credentials credentials;
-//  @In("#{boundaryHelper}")
-//  BoundaryHelper bhelper;
 
-  public void checkLogin() {
-    // user may already be logged in - check
-    final boolean isLoggedIn = identity.isLoggedIn();
-    
-    if (isLoggedIn) {
-      return;
-    }
-    authenticate();
-  }
-  
-  public boolean authenticate() {
-    
-    FacesContext facesContext = FacesContext.getCurrentInstance();
-    ExternalContext ctx = facesContext.getExternalContext();
-    
-    Principal rawPrincipal = ctx.getUserPrincipal();
-    String username = rawPrincipal.getName();
-    
-    try {
-      credentials.setUsername(username);
-      credentials.setPassword(username);
-//      bhelper.setUsername(username);
-      identity.acceptExternallyAuthenticatedPrincipal(ctx.getUserPrincipal());
-      identity.authenticate();
-      
-      /*
-       * CARGAR ROLES
-       */
-      /*EscilaWrapper wrapper = WrapperBuilder.build("saru");
-      DirectoryUserDTO user = null;
-      wrapper.authorize(this.credentials.getUsername());
-      user = wrapper.getUserData();
-      List<PermissionDTO> permissions = wrapper.permissions(username);
-      
-      if(permissions == null || permissions.isEmpty())
-    	  throw new Exception("No tiene permisos para utilizar este recurso");
-      
-      for (PermissionDTO p : permissions) {
-        identity.addRole(p.getPropertyValue());
-      }*/
-      
-      return true;
-    } catch (Exception e) {
-      this.log.error("authenticating Error {0}", e.getMessage());
-      FacesMessages.instance().add(Severity.ERROR, e.getMessage());
-      return false;
-    }
-    
-  }
-  
-  public void logout() {
-    Identity identity = Identity.instance();
-    identity.isLoggedIn(false);
-    identity.logout();
-    // Session.instance().invalidate();
-    
-    /*FacesContext facesContext = FacesContext.getCurrentInstance();
-    ExternalContext ctx = facesContext.getExternalContext();
-    
-    try {
-      String url = System.getProperty("cas.server.url") + "/cas/logout";
-      ctx.redirect(ctx.encodeResourceURL(url));
-    } catch (Exception e) {
-     
-    }*/
-    FacesContext.getCurrentInstance().responseComplete();
-  }
-  
+	@Logger
+	private Log log;
+	@In
+	Identity identity;
+	@In
+	Credentials credentials;
+
+	// @In("#{boundaryHelper}")
+	// BoundaryHelper bhelper;
+
+	public void checkLogin() {
+		// user may already be logged in - check
+		final boolean isLoggedIn = identity.isLoggedIn();
+
+		if (isLoggedIn) {
+			return;
+		}
+		authenticate();
+	}
+
+	public boolean authenticate() {
+
+//		FacesContext facesContext = FacesContext.getCurrentInstance();
+//		ExternalContext ctx = facesContext.getExternalContext();
+		
+		Principal rawPrincipal = new Principal() {
+			
+			@Override
+			public String getName() {
+				return "user";
+			}
+		};
+		
+//		Principal rawPrincipal = ctx.getUserPrincipal();
+		String username = rawPrincipal.getName();
+
+		try {
+			credentials.setUsername(username);
+			credentials.setPassword(username);
+//			bhelper.setUsername(username);
+			identity.acceptExternallyAuthenticatedPrincipal(rawPrincipal);
+			identity.authenticate();
+
+			/*
+			 * CARGAR ROLES
+			 */
+			/*
+			 * EscilaWrapper wrapper = WrapperBuilder.build("saru");
+			 * DirectoryUserDTO user = null;
+			 * wrapper.authorize(this.credentials.getUsername()); user =
+			 * wrapper.getUserData(); List<PermissionDTO> permissions =
+			 * wrapper.permissions(username);
+			 * 
+			 * if(permissions == null || permissions.isEmpty()) throw new
+			 * Exception("No tiene permisos para utilizar este recurso");
+			 * 
+			 * for (PermissionDTO p : permissions) {
+			 * identity.addRole(p.getPropertyValue()); }
+			 */
+
+			return true;
+		} catch (Exception e) {
+			log.error("authenticating Error {0}", e.getMessage());
+			log.error(e.getMessage(), e);
+			FacesMessages.instance().add(Severity.ERROR, e.getMessage());
+			return false;
+		}
+
+	}
+
+	public void logout() {
+		Identity identity = Identity.instance();
+		identity.isLoggedIn(false);
+		identity.logout();
+		// Session.instance().invalidate();
+
+		/*
+		 * FacesContext facesContext = FacesContext.getCurrentInstance();
+		 * ExternalContext ctx = facesContext.getExternalContext();
+		 * 
+		 * try { String url = System.getProperty("cas.server.url") +
+		 * "/cas/logout"; ctx.redirect(ctx.encodeResourceURL(url)); } catch
+		 * (Exception e) {
+		 * 
+		 * }
+		 */
+		FacesContext.getCurrentInstance().responseComplete();
+	}
+
 }

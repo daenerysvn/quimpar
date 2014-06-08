3636a325311c9beb6c19bacb157749f25f2c3533
prueba <prueba@HP-Raul>
2014-06-08 19:12:38 -0400

    domingo

--- src/py/com/quimpar/persistence/dao/ProveedoresDAO.java
+++ src/py/com/quimpar/persistence/dao/ProveedoresDAO.java
@@ -6,6 +6,7 @@ import py.com.quimpar.persistence.dto.ProveedoresDTO;
 public interface ProveedoresDAO {
   List<ProveedoresDTO> listProveedores();
   ProveedoresDTO getProveedoresById(Long id);
+  ProveedoresDTO getProveedoresByRuc(String ruc);
   int createProveedores(ProveedoresDTO dto);
   int deleteProveedores(ProveedoresDTO dto);
   int updateProveedores(ProveedoresDTO dto);
